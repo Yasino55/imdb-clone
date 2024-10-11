764c2eedea8987e8a9c36ab96a8c765899c55a60
Yasin <99790020+Yasino55@users.noreply.github.com>
2024-10-11 15:10:52 +0200

    Added a simple route authentication

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,27 @@
 import NextAuth from "next-auth";
 import authConfig from "./auth.config";
+import { authRoutes, apiAuthRoutes, DEFAULT_LOGIN_REDIRECT } from "./routes";
 
-export const { auth: middleware } = NextAuth(authConfig);
+export const { auth } = NextAuth(authConfig);
+
+export default auth((req) => {
+  const { nextUrl } = req;
+  const isLoggedIn = !!req.auth;
+
+  const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthRoutes);
+  const isAuthRoute = authRoutes.includes(nextUrl.pathname);
+
+  if (isApiAuthRoute) {
+    return undefined;
+  }
+
+  if (isAuthRoute) {
+    if (isLoggedIn) {
+      return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
+    }
+    return undefined;
+  }
+});
 
 export const config = {
   matcher: ["/((?!api|_next/static|_next/image|.*\\.png$).*)"],
