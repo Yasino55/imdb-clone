9a9251d499cb910b465e63cbdcc43e3f9e1ae427
Yasin <99790020+Yasino55@users.noreply.github.com>
2025-01-20 01:53:16 +0100

    fix shadcn theme-prover import error

--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -1,15 +1,11 @@
 "use client";
 
 import * as React from "react";
-const NextThemesProvider = dynamic(
-  () => import("next-themes").then((e) => e.ThemeProvider),
-  {
-    ssr: false,
-  }
-);
-import { type ThemeProviderProps } from "next-themes/dist/types";
-import dynamic from "next/dynamic";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
 
-export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
+export function ThemeProvider({
+  children,
+  ...props
+}: React.ComponentProps<typeof NextThemesProvider>) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
 }
